00630c618174d939cf1735782e3ec359bc76f4be
劉宸睿 <crliu@liuchenruide-MacBook-Pro.local>
2020-09-22 14:34:38 +0800

    Add macOS notification

--- crawler.py
+++ crawler.py
@@ -3,6 +3,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
+import os
+import sys
+import atexit
 
 chrome_options = Options() # 啟動無頭模式
 chrome_options.add_argument('--headless')  #規避google bug
@@ -16,6 +19,16 @@ time.sleep(2)
 
 driver.find_element_by_xpath("//*[@id='table02']/form[1]/table/tbody/tr/td/font/input[4]").send_keys(Keys.SPACE)
 
+def notify(title, text):
+    os.system("""
+        osascript -e 'display notification "{}" with title "{}" '
+        """.format(text, title))
+
+def cleanup_function():
+    notify("Crawler terminated", sys._getframe().f_code.co_name)
+
+atexit.register(cleanup_function)
+
 while True:
     num_rows = len(driver.find_elements_by_xpath("//*[@id='tab2']/tr"))
     num_cols = len(driver.find_elements_by_xpath("//*[@id='tab2']/tr[1]/td"))
@@ -27,13 +40,20 @@ while True:
     for i in range(num_rows):
         row = []
         for j in range(num_cols):
-            FinalXPath = before_XPath + str(i+1) + aftertd_XPath + str(j+1) + aftertr_XPath
-            cell_text = driver.find_element_by_xpath(FinalXPath).text
-            if cell_text == "6645":
-                #Do something
-                print("散戶要來啦！！！")
-            row.append(cell_text)
+            try:
+                FinalXPath = before_XPath + str(i+1) + aftertd_XPath + str(j+1)+aftertr_XPath
+                cell_text = driver.find_element_by_xpath(FinalXPath).text
+                if cell_text == "6645":
+                    #Do something
+                    print("散戶要來啦！！！")
+                    notify("IPTIF announced", "散戶要來啦")
+                row.append(cell_text)
+            except:
+                pass
         print(row)
    
     print("Please hold 20 seconds") 
     time.sleep(20)
+
+
+notify("Title", "Heres an alert", "default")
